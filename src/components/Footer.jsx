2d0c4f863554b35c95c82e12b627d83f8871c1a0
amit biswas <amit.biswas54@gmail.com>
2024-11-21 13:37:47 +0530

    first step to deplo116

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,28 @@
 import footershape from '../../src/external/assets/img/footer-shape.png'
 import Brand from './Brand';
 
+
+
+import '../assets/js/vendor/jquary-3.6.0.min.js'
+import '../assets/js/vendor/bootstrap-bundle.js'
+import '../assets/js/vendor/imagesloaded-pkgd.js'
+import '../assets/js/vendor/waypoints.min.js'
+import '../assets/js/vendor/venobox.min.js'   
+import '../assets/js/vendor/odometer.min.js'  
+import '../assets/js/vendor/meanmenu.js' 
+import '../assets/js/vendor/jquery.isotope.js'
+import '../assets/js/vendor/wow.min.js'
+import '../assets/js/vendor/swiper.min.js'
+import '../assets/js/vendor/split-type.min.js'
+import '../assets/js/vendor/gsap.min.js'
+import '../assets/js/vendor/scroll-trigger.min.js'
+import '../assets/js/vendor/scroll-smoother.js'
+import '../assets/js/vendor/jquery.carouselTicker.js'
+import '../assets/js/vendor/nice-select.js'
+import '../assets/js/ajax-form.js'
+import '../assets/js/contact.js'
+import '../assets/js/main.js'
+
 function Footer () {
 
     return(
