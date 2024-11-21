77e09eab93c4a13790ce110ae797f69978837ade
amit biswas <amit.biswas54@gmail.com>
2024-11-21 12:11:08 +0530

    first step to deplo111

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import './assets/js/vendor/jquary-3.6.0.min.js'
-import './assets/js/vendor/bootstrap-bundle.js'
-import './assets/js/vendor/imagesloaded-pkgd.js'
-import './assets/js/vendor/waypoints.min.js'
-import './assets/js/vendor/venobox.min.js'   
-import './assets/js/vendor/odometer.min.js'  
-import './assets/js/vendor/meanmenu.js' 
-import './assets/js/vendor/jquery.isotope.js'
-import './assets/js/vendor/wow.min.js'
-import './assets/js/vendor/swiper.min.js'
-import './assets/js/vendor/split-type.min.js'
-import './assets/js/vendor/gsap.min.js'
-import './assets/js/vendor/scroll-trigger.min.js'
-import './assets/js/vendor/scroll-smoother.js'
-import './assets/js/vendor/jquery.carouselTicker.js'
-import './assets/js/vendor/nice-select.js'
-import './assets/js/ajax-form.js'
-import './assets/js/contact.js'
-import './assets/js/main.js'
+
 
 
 
